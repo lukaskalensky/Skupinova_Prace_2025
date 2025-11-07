d2428499a21468c5cd6c6e74c26c733ab9206727
velkayolanda <tomo.simurda@gmail.com>
2025-11-07 09:25:52 +0100

    aoc 17_part1

--- Textova_hra/main.cpp
+++ Textova_hra/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 // #include "Game.h"
 
+using namespace std;
+
 int main() {
+    cout << "Ahoj zmrde" << endl;
+    cout << "sup\n";
     return 0;
 }
---
+++ Ukoly_7-11-2025/main.cpp
@@ -0,0 +1,34 @@
+//
+// Created by honzi on 7. 11. 2025.
+//
+
+//advent of code 2017 day 15
+
+#include <iostream>
+#include <cstdint>
+
+using namespace std;
+
+const uint64_t FactorA = 16807;
+const uint64_t FactorB = 48271;
+const uint64_t Divisor = 2147483647;
+
+const int pairs = 40000000;
+
+int main()
+{
+    uint64_t a = 591;
+    uint64_t b = 393;
+
+    int zhody = 0;
+    for (int i = 0; i < pairs; i++)
+    {
+        a = (a*FactorA) % (Divisor);
+        b = (b*FactorB) % (Divisor);
+        if ((a & 0xFFFF) ==(b & 0xFFFF))
+        {
+            zhody++;
+        }
+    }
+    cout<<"zhody z part 1: "<<zhody<<endl;
+}
